cc7de273654b7591c77f80c061971e2d79984fd6
Bhagavathi Chirra <bhagavathi.chirra@gmail.com>
2018-07-06 13:04:22 +1000

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,7 +31,13 @@ p6 = Post.create caption: "My dream house", user: u3, image: "https://www.richar
 p7 = Post.create caption: "I have seen this for the first time", user: u3, image: "https://mcacesblogs.files.wordpress.com/2012/11/hand-tree.jpg"
 p8 = Post.create caption: "Colourfull one...", user: u4, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0Hio_u-c7H99BsyrsiL0RQ6s2mlVOaA1_usp3T33oGCJqlcVM4A"
 p9 = Post.create caption: "Biggest house", user: u6, image: "https://d3ljd161w9lywl.cloudfront.net/property/image/R2219091/6976-wiltshire-street-vancouver-bc-3427_262240718-1821856-0_lightbox.jpg"
-p10 = Post.create caption: "My love", user: u2, image: "http://freewallpaper-background.com/wp-content/uploads/2016/06/Cute-Baby-High-Res-10.jpg"
+p10 = Post.create caption: "My love", user: u1, image: "http://freewallpaper-background.com/wp-content/uploads/2016/06/Cute-Baby-High-Res-10.jpg"
+p11 = Post.create caption: "Nature", user: u2, image: "https://images.unsplash.com/photo-1530738433046-b589e02b0e79?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7f0c18680990cfec1f4444070487462e&auto=format&fit=crop&w=1500&q=80"
+p12 = Post.create caption: "Nature", user: u1, image: "https://images.unsplash.com/photo-1530735332962-1767e1cfee10?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=54180e80d16f7f07e0c58563bc5c18f5&auto=format&fit=crop&w=800&q=60"
+p13 = Post.create caption: "Nature", user: u3, image: "https://images.unsplash.com/photo-1530723634302-cacd3ef6f20f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ddef32e75bafbc01a8997c7d4a5c7c72&auto=format&fit=crop&w=800&q=60"
+p14 = Post.create caption: "Nature", user: u1, image: "https://images.unsplash.com/photo-1530759222099-f94d63af70e4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=86ce647e5fb294bf6d1b75bbc66919e2&auto=format&fit=crop&w=800&q=60"
+p15 = Post.create caption: "Nature", user: u2, image: "https://images.unsplash.com/photo-1530728606679-db4cf2cc479b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=21c210574e6c39ae9d20cc1abab1dddd&auto=format&fit=crop&w=800&q=60"
+p16 = Post.create caption: "Nature", user: u3, image: "https://images.unsplash.com/photo-1530711622124-44932baf6364?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5ad50f971a3e44ed67ad20529fbb1dca&auto=format&fit=crop&w=800&q=60"
 
 
 puts "Created #{Post.all.length} posts."
@@ -55,5 +61,9 @@ c13 = Comment.create content: 'Me too!!!', post: p5, user: u6
 c14 = Comment.create content: 'Ya good', post: p5, user: u9
 c15 = Comment.create content: 'I too like this pic', post: p5, user: u4
 c16 = Comment.create content: 'nice click!!!', post: p10, user: u5
+c17 = Comment.create content: 'I like this', post: p12, user: u5
+c18 = Comment.create content: 'Ya, nice click!!!', post: p13, user: u5
+c19 = Comment.create content: 'So cool...', post: p14, user: u5
+
 
 puts "Created #{Comment.all.length} comments."
